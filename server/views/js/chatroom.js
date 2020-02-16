f13649b1914a6a71d4435413e39f482119194067
Daniel Fiala <danielfialaa@gmail.com>
2020-02-16 17:39:58 +0100

    init

--- server/views/js/chatroom.js
+++ server/views/js/chatroom.js
@@ -1,18 +1,11 @@
 const msgForm = document.getElementById("send-msg-container");
 const msgInput = document.getElementById("msg-input");
-const socket = io("http://localhost:8000");
 
-
-socket.on("chat-message", (msg, from) => {
-  appendMsg(msg, "Someone", true);
-});
 
 msgForm.addEventListener("submit", e => {
   e.preventDefault();
   let msgInput = document.getElementById("msg-input");
   console.log(msgInput.value);
-  socket.emit("send-chat-message", msgInput.value);
-  // socket.emit("typing", room, false);
   appendMsg(msgInput.value, 'You', false);
   msgInput.value = "";
 });
