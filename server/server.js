771ea23aad51c3af4565fca187f015c33959464b
Daniel Fiala <danielfialaa@gmail.com>
2020-03-08 17:24:11 +0100

    init

--- server/server.js
+++ server/server.js
@@ -5,6 +5,16 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+
+
+const initPassport = require('./passport-config');
+initPassport(passport, 
+    name => users.find(user => user.name === name),
+    id => users.find(user => user.id === id)    
+);
 
 
 server.listen(8000);
@@ -13,6 +23,15 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.get("/", (req,res) => {
     res.render('index.ejs');
@@ -59,6 +78,7 @@ app.post('/register', async (req,res) => {
     try{
         const hashedPassword = await bcrypt.hash(req.body.password, 5);
         users.push({
+            id: Date.now().toString(),
             name: req.body.name,
             password: hashedPassword
         });
@@ -68,3 +88,8 @@ app.post('/register', async (req,res) => {
     }
     console.log(users);
 });
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
